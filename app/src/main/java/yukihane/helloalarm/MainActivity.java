fe721e53e178b1b67f177586cc0d5852ef19759f
yukihane <yukihane.feather@gmail.com>
2015-08-03 15:29:50 +0900

    ボタンのイベントハンドラ実装

--- app/src/main/java/yukihane/helloalarm/MainActivity.java
+++ app/src/main/java/yukihane/helloalarm/MainActivity.java
@@ -1,9 +1,17 @@
 package yukihane.helloalarm;
 
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FilenameFilter;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,27 +19,54 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button startButton = (Button) findViewById(R.id.startBtn);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "アラームをセットします", Toast.LENGTH_SHORT);
+                MyAlarm.set(getApplicationContext());
+            }
+        });
+
+        Button stopButton = (Button) findViewById(R.id.stopBtn);
+        stopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "アラームをキャンセルします", Toast.LENGTH_SHORT);
+                MyAlarm.cancel(getApplicationContext());
+            }
+        });
+
+        Button outputLogButton = (Button) findViewById(R.id.logBtn);
+        outputLogButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                exportLog();
+            }
+        });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    private void exportLog() {
+        File filesDir = getFilesDir();
+        File downloadDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+        Toast.makeText(this, downloadDir.toString(), Toast.LENGTH_LONG);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        File[] logfiles = filesDir.listFiles(new FilenameFilter() {
+            @Override
+            public boolean accept(File file, String s) {
+                if (s.endsWith("log") || s.endsWith("log.txt")) {
+                    return true;
+                }
+                return false;
+            }
+        });
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        for (File f : logfiles) {
+            try {
+                Utils.copy(f, downloadDir);
+            } catch (IOException e) {
+            }
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
